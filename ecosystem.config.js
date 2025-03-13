9796500aef15ff7b4f3011573c05c5d723f94fdd
Your Name <143980070+MJBeltran13@users.noreply.github.com>
2025-03-13 15:28:56 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,11 @@ module.exports = {
     {
       name: "ai_chatbot_shop",
       script: "main.py",
-      interpreter: "/usr/bin/python3",
+      interpreter: "/home/bsuadmin/marcjames/ai_chatbot_shop/venv/bin/python3",
       env: {
         PORT: "1551",
+        PYTHONPATH:
+          "/home/bsuadmin/marcjames/ai_chatbot_shop/venv/lib/python3.8/site-packages",
       },
       watch: true,
     },
