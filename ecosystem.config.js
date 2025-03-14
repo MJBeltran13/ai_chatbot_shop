7cb0f1144f1aab562f574b37fd9a9932bd3b973f
Your Name <143980070+MJBeltran13@users.noreply.github.com>
2025-03-14 12:02:14 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: "pombot",
-      script: "/usr/local/bin/gunicorn",
+      script: "/home/bsuadmin/marcjames/ai_chatbot_shop/venv/bin/gunicorn",
       args: "main:app -c gunicorn.conf.py",
+      cwd: "/home/bsuadmin/marcjames/ai_chatbot_shop",
       interpreter: null,
       instances: 1,
       exec_mode: "fork",
@@ -11,6 +12,7 @@ module.exports = {
       env: {
         PYTHONUNBUFFERED: "true",
         PYTHONPATH: "/home/bsuadmin/marcjames/ai_chatbot_shop",
+        PATH: "/home/bsuadmin/marcjames/ai_chatbot_shop/venv/bin:$PATH",
       },
       error_file: "logs/err.log",
       out_file: "logs/out.log",
